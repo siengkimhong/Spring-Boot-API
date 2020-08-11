b08b371ccc935821e7f3feb17c8e8dec49f85f7a
Kim <kimhongora@gmail.com>
2020-08-11 17:08:23 +0700

    API basic Auth

--- src/main/java/com/kimhong/apispring/configuration/SecurityConfiguration.java
+++ src/main/java/com/kimhong/apispring/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.kimhong.apispring.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint;
+
+    @Autowired
+    public void setCustomBasicAuthenticationEntryPoint(CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint) {
+        this.customBasicAuthenticationEntryPoint = customBasicAuthenticationEntryPoint;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -17,7 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 //.antMatchers("/api/**").permitAll()
                 .anyRequest().authenticated();
 
-        http.httpBasic();
+        http.httpBasic().authenticationEntryPoint(customBasicAuthenticationEntryPoint);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
